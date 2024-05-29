7b6121bda26bc6abd0dc31f04049bbde0f2f6d9d
Ayahiro-Inatsugu <aya13001060ina@gmail.com>
2024-05-29 23:08:21 +0900

    fix：演算選択の文面を英語に変更

--- frontend/src/components/features/top/PlainInputTextBox.tsx
+++ frontend/src/components/features/top/PlainInputTextBox.tsx
@@ -4,13 +4,13 @@ type Props = {
   handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const PlainInputTextBox: FC<Props> = ({ handleInputLeftChange, handleInputRightChange }: Props) => {
+const PlainInputTextBox: FC<Props> = ({ inputLeft, inputRight, handleInputLeftChange, handleInputRightChange }: Props) => {
   return (
     <div className="w-full flex flex-col items-center gap-4">
       <p className="text-2xl">Plain Input</p>
       <div className="w-3/5 flex items-center justify-center gap-8">
-        <textarea className="w-1/2 h-16 bg-white rounded-lg text-black p-2" onChange={handleInputLeftChange}></textarea>
-        <textarea className="w-1/2 h-16 bg-white rounded-lg text-black p-2" onChange={handleInputRightChange}></textarea>
+        <textarea className="w-1/2 h-16 bg-white rounded-lg text-black p-2" value={inputLeft} onChange={handleInputLeftChange}></textarea>
+        <textarea className="w-1/2 h-16 bg-white rounded-lg text-black p-2" value={inputRight} onChange={handleInputRightChange}></textarea>
       </div>
     </div>
   )
